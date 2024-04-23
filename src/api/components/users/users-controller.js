2880814b537e9b43c5a5bb79dbbd9b70a7ab9f67
jclynviaa <jocelyn.535230046@stu.untar.ac.id>
2024-04-23 20:15:39 +0700

    mengubah endpoint /users (menambahkan fitur pagination, search, sort)

--- src/api/components/users/users-controller.js
+++ src/api/components/users/users-controller.js
@@ -7,11 +7,44 @@ const { errorResponder, errorTypes } = require('../../../core/errors');
  * @param {object} response - Express response object
  * @param {object} next - Express route middlewares
  * @returns {object} Response object or pass an error to the next route
+ * @param {integer} page_number - Nomor halaman yang ditampilkan
+ * @param {integer} page_size - Jumlah data yang dimunculkan per halaman
+ * @param {string} search - Mencari kata kunci
+ * @param {string} sort - Urutan pengurutan (asc)
+ *
  */
 async function getUsers(request, response, next) {
   try {
-    const users = await usersService.getUsers();
-    return response.status(200).json(users);
+    // pagination, sorting, search
+    const page_n = parseInt(req.query.page_number) || 1;
+    const page_s = parseInt(req.query.page_size) || 10;
+    const search = req.query.search;
+    const sort = req.query.sort || 'email : asc';
+
+    // data users
+    const { users, count } = await usersService.getUsers(
+      page_n,
+      page_s,
+      sort,
+      search
+    );
+
+    // jumlah data yang dimunculkan
+    const t_pages = Math.ceil(count / page_n);
+
+    // menunjukkan apakah ada halaman sebelumnya/selanjutnya
+    const has_p_page = page_n > 1;
+    const has_n_page = page_n < t_pages;
+
+    return response.status(200).json({
+      page_number: page_n,
+      page_size: page_s,
+      count,
+      total_pages: t_pages,
+      has_previous_page: has_p_page,
+      has_next_page: has_n_page,
+      data: users,
+    });
   } catch (error) {
     return next(error);
   }
