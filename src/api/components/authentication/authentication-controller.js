406efeaaca7e88377e34626cc423d4fd521250d8
jclynviaa <jocelyn.535230046@stu.untar.ac.id>
2024-04-27 16:40:05 +0700

    edit login di authentication controller

--- src/api/components/authentication/authentication-controller.js
+++ src/api/components/authentication/authentication-controller.js
@@ -27,6 +27,9 @@ async function login(request, response, next) {
 
     return response.status(200).json(loginSuccess);
   } catch (error) {
+    if (error.type === errorTypes.TOO_MANT_LOGIN_ATTEMPTS) {
+      authenticationServices.resetfailed_login_attempts(email);
+    }
     return next(error);
   }
 }
