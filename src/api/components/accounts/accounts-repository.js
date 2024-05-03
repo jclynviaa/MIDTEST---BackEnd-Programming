c3f5e005712bfe3bbb98dccb3c98c25c280c44b9
jclynviaa <jocelyn.535230046@stu.untar.ac.id>
2024-05-03 16:18:21 +0700

    lengkapin accounts-repository

--- src/api/components/accounts/accounts-repository.js
+++ src/api/components/accounts/accounts-repository.js
@@ -1,4 +1,4 @@
-const { Account } = require('../../../models');
+const { Account, User } = require('../../../models');
 const { account_number } = require('../../../models/accounts-schema');
 const accountsValidator = require('./accounts-validator');
 
@@ -40,24 +40,103 @@ async function get_account_by_number(account_number) {
 }
 
 /**
- *
+ * Get list of customers
  * @param {*} account_number - Account Number
  * @returns
  */
-async function get_account(account_number) {
-  return Account.findOne({ account_number });
+async function get_customers() {
+  return customers;
 }
 
-async function update_account() {
-  return Account.updateOne({});
+/**
+ *
+ * @param {*} id
+ * @param {*} account_number
+ * @param {*} customer_name
+ * @param {*} customer_id
+ * @param {*} customer_contact
+ * @param {*} initial_deposit
+ * @returns
+ */
+async function update_account(
+  id,
+  account_number,
+  customer_name,
+  customer_id,
+  customer_contact,
+  initial_deposit
+) {
+  return Account.updateOne(
+    {
+      _id: id,
+    },
+    {
+      $set: {
+        account_number,
+        customer_name,
+        customer_id,
+        customer_contact,
+        initial_deposit,
+      },
+    }
+  );
 }
 
-async function delete_account() {}
+/**
+ *
+ * @param {*} id
+ * @param {*} transaction_amount
+ * @param {*} description
+ * @returns
+ */
+async function update_transactions(id, transaction_amount, description) {
+  return Account.updateOne(
+    {
+      _id: id,
+    },
+    {
+      $set: {
+        transaction_amount,
+        description,
+      },
+    }
+  );
+}
+
+/**
+ *
+ * @param {*} id
+ * @param {*} account_number
+ * @returns
+ */
+async function update_balance(id, account_number) {
+  return Account.updateOne(
+    {
+      _id: id,
+    },
+    {
+      $set: {
+        account_number,
+      },
+    }
+  );
+}
+
+async function delete_account(account_number) {
+  return User.deleteOne({ account_number });
+}
+
+async function delete_transactions(transaction_id) {
+  return User.deleteOne({ transaction_id });
+}
 
 module.exports = {
   create_account,
   get_account_by_number,
-  get_account,
+  get_customers,
   update_account,
+  update_transactions,
+  update_balance,
   delete_account,
+  delete_transactions,
 };
